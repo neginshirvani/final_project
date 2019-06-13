a6b8cbe7aeba9e8de239649ca6988d5e118f1a7a
a-samani <50885611+a-samani@users.noreply.github.com>
2019-06-13 20:16:13 +0430

    read file

---
+++ read file.c
@@ -0,0 +1,34 @@
+# final_project
+#include<stdio.h>
+#include <stdlib.h>
+char array[1000][1000];
+int main () {
+    FILE *map;
+    map = fopen("map-pacman.txt", "r");
+    char c;
+    int i = 0;
+    int j = 0;
+    while ((c = fgetc(map)) != EOF) {
+        array[i][j] = c;
+        ++j;
+        if (c == '\n') {
+            ++i;
+            j = 0;
+        }
+    }
+    fclose(map);
+    array[i][j] = '\0';
+    i = j = 0;
+    int ch;
+    while(array[i][j]!='\0'){
+
+        putchar(array[i][j]);
+        ch=array[i][j];
+        ++j;
+        if(ch=='\n'){
+            ++i;
+            j=0;
+        }
+    }
+}
+
