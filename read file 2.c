ff3972e5ea8679b72fd6fdae1f9d5d32ec708306
Ali Samani <alisamani1510@yahoo.com>
2019-06-16 02:22:20 +0430

    readactionket

--- read file 2.c
+++ read file 2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct data{
+    char name[20];
+    char action;
 
+};
+struct node{
+    struct data key;
+    struct node *head=NULL;
+}actkey;
+int main(){
+
+}
