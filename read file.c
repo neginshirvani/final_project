9351e50b5c8e99ed545f4eba54524b19b4626b85
a-samani <50885611+a-samani@users.noreply.github.com>
2019-06-15 06:18:36 +0430

    file read ok

--- read file.c
+++ read file.c
@@ -19,6 +19,13 @@ int main () {
     fclose(map);
     array[i][j] = '\0';
     i = j = 0;
+    for (j=0;array[i][j]!='#';++j){   // finding first block of map
+        if(array[i][j]=='\n'){
+            ++i;
+            j=0;
+        }
+    }
+    j=0;
     int ch;
     while(array[i][j]!='\0'){
 
